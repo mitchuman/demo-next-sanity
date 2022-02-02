4f64345ac2623e80e4b835517b7e186051efb538
Mitchell Christ <mitchell@human.marketing>
2022-02-02 12:30:21 -0800

    start sanity

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,25 @@
-export default () => (
-  <h1>Home</h1>
-)
+import { client } from 'lib/sanity'
+
+const IndexPg = ({ humans }) => {
+  return <>
+    <h1>Home</h1>
+
+    <ul>
+      {humans?.map(human => (
+        <li>{human.name}</li>
+      ))}
+    </ul>
+  </>
+}
+
+export default IndexPg
+
+export async function getStaticProps() {
+  const humans = await client.fetch(`*[_type == "human"]`)
+
+  return {
+    props: {
+      humans,
+    }
+  }
+}
